7f8c83851bbe93961313ade569abf9acfc730416
wathsan11 <wathsaninduwara@gmail.com>
2025-11-11 19:23:06 +0530

    Add stage for building Docker image in Jenkins pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -10,6 +10,23 @@ pipeline{
                 }
             }
         }
+
+        stage('Build Docker Image') {
+            steps {
+                script {
+                    // Define image name and tag
+                    def imageName = "wathsan/diaryapp:${env.BUILD_NUMBER}"
+
+                    echo "Building Docker image: ${imageName}"
+
+                    // Build image
+                    sh "docker build -t ${imageName} ."
+
+                    // Save it to a variable if you need it later (for push, deploy, etc.)
+                    dockerImage = docker.image(imageName)
+                }
+            }
+        }
     }
 
 }
